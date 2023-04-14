f4817fe733414d067cf26176f7ce32c33e3f4eba
ventureful <hades0122@outlook.com>
2023-04-15 00:33:45 +0200

    Added CRUD for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  resources :articles
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "welcome#index"
+  root "articles#index"
   get 'about', to: 'welcome#about'
-  get 'news', to: 'articles#news'
-  get 'reviews', to: 'articles#reviews'
-  get 'advices', to: 'articles#advices'
-  get 'other', to: 'articles#other'
+  get 'news', to: 'articles#index'
+  get 'reviews', to: 'articles#index'
+  get 'advices', to: 'articles#index'
+  get 'other', to: 'articles#index'
 end
