70c47415c5149d15eb427b8f064f8f5f451a05ab
ventureful <hades0122@outlook.com>
2023-04-17 23:37:58 +0200

    Added Rich text for Article

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,8 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# application.js, application.scss, and all non-JS/CSS in the app/assets
+# legacyapplication.js, application.scss, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( jquery.min.js jquery_ujs.js bootstrap-sprockets.js bootstrap.min.js popper.js actiontext.js)
+Rails.application.config.assets.precompile += %w( application.scss )
